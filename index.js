285957ed9e7662343375527fd5f9cc1424dfd8cf
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-19 00:40:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(toDo = "roller-skate", toDo2 = "bathe my dog") {
+    return `This Saturday, I want to ${toDo}!`;
+    return `This Saturday, I want to ${toDo2}!`;
+}
+
+  const mondayWork = function(toDo = "go to the office", toDo2 = "work from home") {
+    return `This Monday, I will ${toDo}.`;
+    return `This Monday, I will ${toDo2}.`;
+  }
+
+function wrapAdjective(highlight = "*") {
+  return function(adjective = "a hard worker") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  }
+}
+
+  function wrapAdjective(highlight = "||") {
+    return function(adjective = "a dedicated programmer" ){
+    return `You are ${highlight}${adjective}${highlight}!`;
+  }
+}
